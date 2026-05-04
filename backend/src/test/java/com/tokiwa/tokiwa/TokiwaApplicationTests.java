98df47d10cd6dc2f016c71069102181de7c66dbc
JuanDaMunoz <jmunoz385@uan.edu.co>
2026-05-04 16:41:09 -0500

    Proyecto base Spring Boot con endpoint /test funcionando

---
+++ backend/src/test/java/com/tokiwa/tokiwa/TokiwaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tokiwa.tokiwa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TokiwaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
