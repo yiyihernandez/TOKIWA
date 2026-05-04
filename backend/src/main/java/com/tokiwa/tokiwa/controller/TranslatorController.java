94558b998dceb6a523d266282ef57ac6e03c989e
JuanDaMunoz <jmunoz385@uan.edu.co>
2026-05-04 18:33:19 -0500

    se le agrego el resto del backend

--- backend/src/main/java/com/tokiwa/tokiwa/controller/TranslatorController.java
+++ backend/src/main/java/com/tokiwa/tokiwa/controller/TranslatorController.java
@@ -4,16 +4,17 @@ import com.tokiwa.tokiwa.model.TranslationRequest;
 import com.tokiwa.tokiwa.model.TranslationResponse;
 import com.tokiwa.tokiwa.service.TranslatorService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.;
 
 @RestController
-@RequestMapping("/api")
+@CrossOrigin(origins = "") // Permite que tu compañero se conecte desde su PC
+@RequestMapping("/api")      // Base de la URL
 public class TranslatorController {
 
     @Autowired
     private TranslatorService translatorService;
 
-    @PostMapping("/traducir")
+    @PostMapping("/traducir") // Ruta final del endpoint
     public TranslationResponse traducir(@RequestBody TranslationRequest request) {
         return translatorService.traducir(request);
     }
