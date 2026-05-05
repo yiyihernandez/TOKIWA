46a1af17eef8a5587943ede6f141b3eeba0274a2
JuanDaMunoz <jmunoz385@uan.edu.co>
2026-05-04 19:07:13 -0500

    feat: configuracion CORS global y fix imports en TranslatorController

--- backend/src/main/java/com/tokiwa/tokiwa/controller/TranslatorController.java
+++ backend/src/main/java/com/tokiwa/tokiwa/controller/TranslatorController.java
@@ -4,17 +4,21 @@ import com.tokiwa.tokiwa.model.TranslationRequest;
 import com.tokiwa.tokiwa.model.TranslationResponse;
 import com.tokiwa.tokiwa.service.TranslatorService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = "") // Permite que tu compañero se conecte desde su PC
-@RequestMapping("/api")      // Base de la URL
+@CrossOrigin(origins = "*")
+@RequestMapping("/api")
 public class TranslatorController {
 
     @Autowired
     private TranslatorService translatorService;
 
-    @PostMapping("/traducir") // Ruta final del endpoint
+    @PostMapping("/traducir")
     public TranslationResponse traducir(@RequestBody TranslationRequest request) {
         return translatorService.traducir(request);
     }
